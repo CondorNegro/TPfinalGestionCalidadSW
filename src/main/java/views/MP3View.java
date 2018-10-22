165494d87adce679b9f414135f896fa4d2fb2ec7
lopezgaston <lopez.gaston.1996@gmail.com>
2018-10-22 02:09:11 -0300

    mp3view prueba findbugs

--- src/main/java/views/MP3View.java
+++ src/main/java/views/MP3View.java
@@ -378,7 +378,6 @@ public class MP3View extends JFrame implements ActionListener, TrackObserver, Pr
 	@Override
 	public void updateTrackProgress(int progress, int size) {
 		int variableSinUsarFindBugs;
-		
 		if (progress==0){
 			progressBar.reset();
 		}
