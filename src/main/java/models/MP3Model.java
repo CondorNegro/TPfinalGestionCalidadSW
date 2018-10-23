926cfef8fa7a32c50c5be5a3a36d8e0d535fd83a
lopezgaston <lopez.gaston.1996@gmail.com>
2018-10-23 13:52:19 -0300

    pruebo findbugs

--- src/main/java/models/MP3Model.java
+++ src/main/java/models/MP3Model.java
@@ -289,9 +289,9 @@ public class MP3Model implements MP3ModelInterface {
 			byte[] albumArt = song.getId3v2Tag().getAlbumImage();
 			return albumArt;
 		}
-		//byte[] error = "Error".getBytes();
-		//return error;
-		return null;
+		byte[] error = "Error".getBytes();
+		return error;
+		//return null;
 	}
 	
 	@Override
